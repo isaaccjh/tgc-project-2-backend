0bf921864bb8e6f51de11c6b45e7fb43973d35ea
Isaac <isaacforschool05@gmail.com>
2023-03-16 10:36:06 +0800

    connected mongodb to express

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const MongoClient = require("mongodb").MongoClient;
-const cors = require("cors")
-require("dotenv").config();
-
-const MongoUri = process.env.URI
-
 const express = require("express");
 const app = express();
+
+const cors = require("cors");
+
+// ENABLE PROCESSING JSON DATA
+app.use(express.json());
+// ENABLE CORS
+app.use(cors());
+
+require("dotenv").config();
+const MongoUri = process.env.URI
+
+const MongoClient = require("mongodb").MongoClient;
+
 
 async function connect() {
     const client = await MongoClient.connect(MongoUri,{"useUnifiedTopology": true});
 
     const db = client.db("cocktail");
-    return db;
+
+    // app.get("/", (req, res) => {
+    //     res.send("<h1>Conneted to express</h1>")
+    // });
+    
+    app.get("/", async function(req,res) {
+        const ingredients = await db.collection("ingredients_collection").find({}).limit(10).toArray();
+        res.json(ingredients);
+    })
 }
 
+connect();
 
-
-
-app.get("/", (req, res) => {
-    res.send("<h1>Conneted to express</h1>")
-})
-
-
-
-
-
-app.listen(5500, () => {
+app.listen(5500, ()  => {
     console.log("Server has started");
 })
 
