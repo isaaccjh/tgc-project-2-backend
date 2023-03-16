dd2799ab0a91b44e25003b4535372b215f5cf7d5
Isaac <isaacforschool05@gmail.com>
2023-03-16 15:40:01 +0800

    added search filter (read)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cors = require("cors");
 
-// VALIDATION 
+// VALIDATIONS
 const validatePost = require("./validatePost");
 
 // ENABLE PROCESSING JSON DATA
@@ -21,12 +21,32 @@ async function connect() {
     const client = await MongoClient.connect(MongoUri, { "useUnifiedTopology": true });
 
     const db = client.db("cocktail");
+    
+    // GET ALL COCKTAILS [READ]
+    app.get("/cocktails", async function (req, res) {
+        const filter = {};
+        
+        if (req.query.name) {
+            filter["name"] = req.query.name;
+        };
 
-    app.get("/", async function (req, res) {
-        const cocktail = await db.collection("cocktail_collection").find({}).limit(2).toArray();
+        if (req.query.glassType) {
+            filter["glassType"] = req.query.glassType;
+        }
+
+        if (req.query.distinction) {
+            filter["distinctions"] = {
+                "$regex": req.query.distinction,
+                "$options": "i"
+            }
+        }
+
+        const cocktail = await db.collection("cocktail_collection").find(filter).limit(2).toArray();
         res.json(cocktail);
-    })
 
+    });
+
+    // POST NEW COCKTAIL [CREATE]
     app.post("/new-post", validatePost, async function (req, res) {
         try {
             const result = await db.collection("cocktail_collection")
@@ -51,7 +71,7 @@ async function connect() {
                 "error": "Database not available. Please try again later or contact the developer of this API."
             })
         }
-    })
+    });
 }
 
 connect();
